0ed8bb3560775dbe43cc272bad2a6024f8cbef61
danielcampos <ddregalo@gmail.com>
2018-02-16 12:54:46 +0000

    Add code for running an array of fizzbuzz numbers 1-100

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,14 @@
 def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    "#{number} --> fizzbuzz"
-  elsif number % 3 == 0
-    "#{number} --> fizz"
-  elsif number % 5 == 0
-    "#{number} --> buzz"
-  else
-    "#{number} --> #{number}"
+  (1..number).to_a.each do |number|
+    if number % 3 == 0 && number % 5 == 0
+      puts "#{number} --> fizzbuzz"
+    elsif number % 3 == 0
+      puts "#{number} --> fizz"
+    elsif number % 5 == 0
+      puts "#{number} --> buzz"
+    else
+      puts "#{number} --> #{number}"
+    end
   end
 end
+fizzbuzz(100)
