d9a1c0e11261f9b51605aa6aff3087971ab85c0b
danielcampos <ddregalo@gmail.com>
2018-02-16 12:24:52 +0000

    Write code and tests for fizzbuzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,9 @@
+require 'fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do 
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "45 --> fizzbuzz" when passed 45' do
+    expect(fizzbuzz(45)).to eq '45 --> fizzbuzz'
+  end
+  it 'returns "3 --> fizz" when passed 3' do 
+    expect(fizzbuzz(3)).to eq '3 --> fizz'
   end
 end
