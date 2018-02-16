d0107ee4bd8b95e98251ff7802acd20e56dd8723
danielcampos <ddregalo@gmail.com>
2018-02-16 12:38:04 +0000

    Add buzz and normall number code + tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,4 +6,10 @@ describe 'fizzbuzz' do
   it 'returns "3 --> fizz" when passed 3' do 
     expect(fizzbuzz(3)).to eq '3 --> fizz'
   end
+  it 'returns "5 --> buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq '5 --> buzz'
+  end
+  it 'returns "-2 --> -2" when passed -2' do
+    expect(fizzbuzz(-2)).to eq '-2 --> -2'
+  end
 end
